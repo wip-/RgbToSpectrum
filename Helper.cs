647d69cfe3ab4c708cc89fb1f360851637787586
Wil <wip-@users.noreply.github.com>
2014-03-26 18:44:55 +0900

    Add empty VS solution

---
+++ Helper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Helpers
+{
+    public static class Extensions
+    {
+        //http://stackoverflow.com/a/2683487/758666
+        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
+        {
+            if (val.CompareTo(min) < 0) return min;
+            else if (val.CompareTo(max) > 0) return max;
+            else return val;
+        }   
+
+        public static byte ToByte(this double val)
+        {
+            return Convert.ToByte(val.Clamp(0, 255));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Helpers;
+
+namespace RgbToSpectrum
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool initialized = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            initialized = true;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (!initialized)
+                return;
+
+            byte R = SliderR.Value.ToByte();
+            byte G = SliderG.Value.ToByte();
+            byte B = SliderB.Value.ToByte();
+
+            LabelR.Content = String.Format("R={0:000}", R);
+            LabelG.Content = String.Format("G={0:000}", G);
+            LabelB.Content = String.Format("B={0:000}", B);
+
+            var brush = new SolidColorBrush();
+            brush.Color = Color.FromArgb(255, R, G, B);
+            Background = brush;
+
+            // TODO
+
+            // spectrum = new Spectrum(R, G, B);
+            // bitmap = new SpectrumCurve(spectrum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
